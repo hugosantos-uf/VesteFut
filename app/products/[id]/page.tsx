d1e4d44612a2bc7560bfb14c6b7c38bef017f7e5
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-19 10:59:09 -0300

    added async function to ProductPage

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,6 +1,10 @@
 import ProductContent from "@/app/components/templates/ProductContent";
 
-export default function ProductPage({ params }: { params: { id: string } }) {
+export default async function ProductPage({
+  params,
+}: {
+  params: { id: string };
+}) {
   const id = Number(params.id);
 
   return <ProductContent id={id} />;
