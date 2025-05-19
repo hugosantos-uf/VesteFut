f17072e5cd9133af8332574fc5cd1b8c45031942
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-19 11:02:16 -0300

    fix

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,10 +1,6 @@
 import ProductContent from "@/app/components/templates/ProductContent";
 
-export default async function ProductPage({
-  params,
-}: {
-  params: { id: string };
-}) {
+export default function ProductPage({ params }: { params: { id: string } }) {
   const id = Number(params.id);
 
   return <ProductContent id={id} />;
