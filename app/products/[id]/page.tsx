3aee745aed8fa722fbb095cf3a9a3eabb766f498
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-19 10:55:37 -0300

    fix type

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,17 +1,7 @@
 import ProductContent from "@/app/components/templates/ProductContent";
 
-type Params = {
-  params: {
-    id: string;
-  };
-};
+export default function ProductPage({ params }: { params: { id: string } }) {
+  const id = Number(params.id);
 
-export default function ProductPage({ params }: Params) {
-  const id = Number(params.id); // convertendo de string para number
-
-  return (
-    <>
-      <ProductContent id={id} />
-    </>
-  );
+  return <ProductContent id={id} />;
 }
