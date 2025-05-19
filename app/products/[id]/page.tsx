c6d5c0e5460f27d4032b571cfa22692fe73d8b7b
hugosantos_uf <hugosantos.ufg@gmail.com>
2025-05-19 11:11:47 -0300

    test

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -1,7 +1,5 @@
 import ProductContent from "@/app/components/templates/ProductContent";
 
 export default function ProductPage({ params }: { params: { id: string } }) {
-  const id = Number(params.id);
-
-  return <ProductContent id={id} />;
+  return <ProductContent id={Number(params.id)} />;
 }
